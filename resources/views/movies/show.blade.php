cbd100a5d05f4a31dc0f7401746513e65868aa08
BrianBontempi <brian.bont54@gmail.com>
2024-03-14 15:26:09 +0100

    make main layout

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,37 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+@section('title', $movie->title)
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+@section('main')
 
-    <title>{{ $movie->title }}</title>
-</head>
+<div class="container my-4">
+    <h1 class="text-center text-primary">Questo è il film: {{ $movie->title }} </h1>
+    <div class="row">
+        <div class="col-12 gy-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $movie->title }}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">Original Title:
+                        {{ $movie->original_title }}</h6>
+                    <h6 class="card-subtitle mb-2 text-warning">Nationality: {{ $movie->nationality }}
+                    </h6>
+                    <h6 class="card-subtitle mb-2 text-secondary">Date: {{ $movie->date }}</h6>
+                    <h6 class="card-subtitle mb-2 text-danger">Vote: {{ $movie->vote }}</h6>
 
-<body>
-    <div class="container my-4">
-        <h1 class="text-center text-danger">Questa è la sezione Movie</h1>
-        <div class="row">
-            <div class="col-12 gy-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $movie->title }}</h5>
-                        <h6 class="card-subtitle mb-2 text-body-secondary">Original Title:
-                            {{ $movie->original_title }}</h6>
-                        <h6 class="card-subtitle mb-2 text-warning">Nationality: {{ $movie->nationality }}
-                        </h6>
-                        <h6 class="card-subtitle mb-2 text-secondary">Date: {{ $movie->date }}</h6>
-                        <h6 class="card-subtitle mb-2 text-danger">Vote: {{ $movie->vote }}</h6>
-
-                        <a href="{{ route('home') }}" class="card-link">Vai al film</a>
-                    </div>
+                    <a href="{{ route('home') }}" class="card-link">Vai al film</a>
                 </div>
             </div>
         </div>
     </div>
-</body>
-
-</html>
+</div>
+@endsection
